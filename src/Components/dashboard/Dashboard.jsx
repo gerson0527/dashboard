d3f6e2a4b0ff3ee968a433997af3096f85428970
Gerson Oritz <gortiz@tiresia.co>
2023-11-09 17:03:32 -0500

    se subio la aplicacion

---
+++ src/Components/dashboard/Dashboard.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import'./dashboard.css'
+const Dashboard = () => {
+  return (
+    <div className='container'>
+        <div className="navegation">
+            <ul>
+                <li><a href="">
+                    <span className='icon'><ion-icon name="cash-outline"></ion-icon></span>
+                    <span>Brand name</span>
+                    </a>
+                </li>
+                <li><a href="">
+                    <span className='icon'><ion-icon name="cash-outline"></ion-icon></span>
+                    <span>Brand name</span>
+                    </a>
+                </li>
+                <li><a href="">
+                    <span className='icon'><ion-icon name="cash-outline"></ion-icon></span>
+                    <span>Brand name</span>
+                    </a>
+                </li>
+                <li><a href="">
+                    <span className='icon'><ion-icon name="cash-outline"></ion-icon></span>
+                    <span>Brand name</span>
+                    </a>
+                </li>
+                <li><a href="">
+                    <span className='icon'><ion-icon name="cash-outline"></ion-icon></span>
+                    <span>Brand name</span>
+                    </a>
+                </li>
+                <li><a href="">
+                    <span className='icon'><ion-icon name="cash-outline"></ion-icon></span>
+                    <span>Brand name</span>
+                    </a>
+                </li>
+                <li><a href="">
+                    <span className='icon'><ion-icon name="cash-outline"></ion-icon></span>
+                    <span>Brand name</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
